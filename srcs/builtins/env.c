eae261f50ef3f775e570b67657783e2bc207eb70
Umami Rycherd <urycherd@ga-k4.msk.21-school.ru>
2022-09-09 17:15:10 +0300

    env fix

--- srcs/builtins/env.c
+++ srcs/builtins/env.c
@@ -6,7 +6,7 @@
 /*   By: urycherd <urycherd@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/08 11:08:08 by urycherd          #+#    #+#             */
-/*   Updated: 2022/09/09 17:07:19 by urycherd         ###   ########.fr       */
+/*   Updated: 2022/09/09 17:14:35 by urycherd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int	ft_env(t_list *env)
 		while (env->next != NULL)
 		{
 			ft_putstr_fd(env->content, 1);
-			write(1, '\n', 1)
+			write(1, "\n", 1);
 			env = env->next;
 		}
 		ft_putstr_fd(env->content, 1);

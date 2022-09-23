bf8009b25db9651c470efdf71b3a0744bbcd07dd
Umami Rycherd <urycherd@oa-k3.msk.21-school.ru>
2022-09-23 20:59:53 +0300

    close done but not tested

--- srcs/builtins/exit.c
+++ srcs/builtins/exit.c
@@ -6,38 +6,74 @@
 /*   By: urycherd <urycherd@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/08 11:08:05 by urycherd          #+#    #+#             */
-/*   Updated: 2022/09/22 21:59:21 by urycherd         ###   ########.fr       */
+/*   Updated: 2022/09/23 20:58:19 by urycherd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	ft_strisnum(char *str)
+// ее можно улучшить для этого надо использовать лонг лонг и после этого вписывать в результат от 0 до unsigned max
+
+unsigned int	ft_atou(char *str)
+{
+	unsigned int	num;
+	int				i;
+	int				sign;
+
+	i = 0;
+	num = 0;
+	sign = 1;
+	if (str[0] == '-')
+	{
+		sign *= -1;
+		i++;
+	}
+	while (str[i])
+	{
+		num = num * 10 + (str[i] - '0');
+		i++;
+	}
+	if (sign < 0)
+		num = 4294967296 - num;
+	return (num);
+}
+
+static int	ft_strisnum(char *str)
 {
 	int	i;
 
 	i = 0;
-	while (str)
+	if (str == NULL)
+		return (0);
+	if (str[0] == '-')
+		i++;
+	while (str[i])
 	{
-
+		if (str[i] < '0' || str[i] > '9')
+			return (0);
+		i++;
 	}
+	return (1);
 }
 
 void	ft_exit(t_main *main, char **arg)
 {
-	ft_putstr_fd("exit ", 2);
-	// if more then 1 arg - error
+	ft_putstr_fd("exit\n", 2);
 	if (cmd[2])
-		ft_putendl_fd("minishell: exit: too many arguments", 2);
+	{
+		ft_putstr_fd("minishell: exit: too many arguments\n", 2);
+		return ;
+	}
+	main->exit_f = 1;
 	else if (cmd[1] && ft_strisnum(cmd[1]) == 0)
 	{
-		main->exit_f = 1;
-		main->ret = ft_itou(num) % 256;
+		main->ret = 255;
+		ft_putstr_fd("minishell: exit: too many arguments", 2);
+		ft_putstr_fd(cmd[1], 2);
+		ft_putstr_fd(": numeric argument required\n", 2);
 	}
-	// if arg not num - error
-	else if ()
-	{}
-	else if ()
-	{}
-	// if num and shorter then 10 symbles - i'm takeing module of this num (возвращает в процесс n % 256)
+	else if (cmd[1])
+		main->ret = ft_atou(num) % 256;
+	else
+		mini->ret = 0;
 }

64bba12860550c0f2378131d386d57fba536bc79
Umami Rycherd <urycherd@at-n3.msk.21-school.ru>
2022-10-05 20:39:55 +0300

    some more minishell

--- srcs/executor.c
+++ srcs/executor.c
@@ -6,7 +6,7 @@
 /*   By: urycherd <urycherd@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/03 21:23:36 by urycherd          #+#    #+#             */
-/*   Updated: 2022/10/05 20:05:17 by urycherd         ###   ########.fr       */
+/*   Updated: 2022/10/05 20:38:21 by urycherd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,50 +34,51 @@ int	take_your_builtin(t_main *main)
 	return (-1);
 }
 
-// int	ft_pipex(t_main *main, char **args)
-// {
-// 	t_cmd	*tmp;
-// 	t_cmd	*prev;
-// 	int		fd1[2];
-// 	int		fd_tmp;
-// 	// t_list	*cmd;
-// 	// 	t_ppx	ppx;
+int	ft_pipex(t_main *main, char **args)
+{
+	// t_cmd	*tmp;
+	// t_cmd	*prev;
+	// int		fd1[2];
+	// int		fd_tmp;
 
-// 	// cmd = main->commands;
-// // 	if (pipe(ppx->fd) == -1)
-// // 		ft_error("Error: pipe problems");
-// // 	while (cmd)
-// // 	{
-// // 		dup2(lol->fd[1], 1); // меняем запись вместо stdout (вывод) в pipe
-// // 		close(lol->fd[0]); // закрываю на чтение
-// // 		// dup2(lol->file1, 0); // меняем чтение из фйал вместо stdin (ввод)
+	t_list	*cmd;
+	t_ppx	ppx;
 
+	cmd = main->commands;
+	while (cmd)
+	{
+		if (pipe(ppx.fd) == -1)
+			return (print_error_nocmd(args[0], "pipe doesn't work"));
+		dup2(ppx.fd[1], 1); // меняем запись вместо stdout (вывод) в pipe
+		close(ppx.fd[0]); // закрываю на чтение
+		if (take_your_builtin(main) < 0)
+			ft_excv(main, args);
+		// dup2(ppx->file1, 0); // меняем чтение из фйал вместо stdin (ввод)
 
-// // 		// dup2(lol->fd[0], 0); // меняем чтение вместо stdin (ввод) в pipe
-// // 		// close(lol->fd[1]); // закрываю на запись
-// // 		// dup2(lol->file2, 1); // меняем запись в фйал вместо stdout (вывод)
-// // 		ft_excv(main, args);
-// // 		cmd = cmd->next;
-// // 	}
+		dup2(ppx.fd[0], 0); // меняем чтение вместо stdin (ввод) в pipe
+		close(ppx.fd[1]); // закрываю на запись
+		// dup2(ppx->file2, 1); // меняем запись в фйал вместо stdout (вывод)
+		cmd = cmd->next;
+	}
 
-// 	tmp = main->commands;
-// 	prev = NULL;
-// 	while (tmp != NULL)
-// 	{
-// 		if (pipe(fd1) == -1)
-// 			ft_error("Error: pipe problems");
-// 		if (prev == NULL)
-// 			exec_first_cmd(*env, tmp, fd1);
-// 		else if (((t_list *)tmp)->next == NULL)
-// 			exec_last_cmd(*env, tmp);
-// 		else
-// 			exec_middle_cmd(*env, tmp);
-// 		prev = tmp;
-// 		tmp = tmp->next;
-// 	}
+	// tmp = main->commands;
+	// prev = NULL;
+	// while (tmp != NULL)
+	// {
+	// 	if (pipe(fd1) == -1)
+	// 		ft_error("Error: pipe problems");
+	// 	if (prev == NULL)
+	// 		exec_first_cmd(*env, tmp, fd1);
+	// 	else if (((t_list *)tmp)->next == NULL)
+	// 		exec_last_cmd(*env, tmp);
+	// 	else
+	// 		exec_middle_cmd(*env, tmp);
+	// 	prev = tmp;
+	// 	tmp = tmp->next;
+	// }
 
-// 	return (0);
-// }
+	return (0);
+}
 
 int	executor(t_main *main)
 {
